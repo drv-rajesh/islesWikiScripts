56fd7e8874f9ac4d3525456d7f4925030dfb28ff
Dhruv Rajesh <drv.rajesh98@gmail.com>
2021-11-21 10:29:05 -0500

    Update itemAdder.py

--- bots/itemAdder.py
+++ bots/itemAdder.py
@@ -1,5 +1,5 @@
 import pywikibot as pwb
-import json
+import json, os
 
 #setup
 wik = pwb.Site("en", "isles")
@@ -9,24 +9,44 @@ with open("Items.json") as f: data = json.load(f)
 for ele in data:
     title = ele["meta"].title()
     pg = pwb.Page(wik, title)
+
+    if ele["quest_item"] == True: quest_item = "Yes"
+    else: quest_item = "No"
     
-    #parameters
-    if ele["quest_item"] == True: quest_item = "Yes"; else: quest_item = "No"
-    if ele["transferable"] == True: transferable = "Yes"; else: transferable = "No"
+    if ele["transferable"] == True: transferable = "Yes"
+    else: transferable = "No"
+
     equipable = "no"
-    try: if ele["equipment_slots"]: equipable = "Yes"; except: equipable = "No"
-    if ele["stackable"] == True: stackable = "Yes"; else: stackable = "No"
+    try:
+        if ele["equipment_slots"]: equipable = "Yes"
+    except:
+        equipable = "No"
+        
+    if ele["stackable"] == True: stackable = "Yes"
+    else: stackable = "No"
+
+    if ele["crateable"] == True: crateable = "Yes"
+    else: crateable = "No"
+
     quality = ele["quality"].title()
-    value = ele["value"]; if value == -1: value = 0
+    if quality in ["Resource", "Consumable", "Quest", "Utility", "Ammo"]:
+        quality = "None"
+    
+    value = ele["value"]
+
+    if value == -1:
+        value = 0
 
     text = "{{ItemBlock"
     text += "|questItem=" + quest_item
-    text += "|tradeable="+ transferable; text += "|equipable="+ equipable; text += "|stackable="+ stackable
+    text += "|tradeable="+ transferable
+    text += "|equipable="+ equipable
+    text += "|stackable="+ stackable
     text += "|rarity="+ quality
     text += "|value="+ str(value)
     text += "}}"
 
-    text += "\n" + "=== Store Locations ===" + "\n" + "{{StoreLocations}}"; text += "\n" + "=== Drop Locations ===" + "\n" + "{{DropLocations}}"
+    temp = pg.text[pg.text.index("{"):pg.text.index("}")+2]
+    pg.text = pg.text.replace(temp, text)
 
-    pg.text = text
     pg.save("Items commit")
